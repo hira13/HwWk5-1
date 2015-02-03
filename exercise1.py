ad6d35d78089c18133d32666137fd5924d233e0e
hirangi <hira13@gmail.com>
2015-02-03 01:43:44 +0000

    Added the trimmed text and then trimmed the dna sequence length and printed it

--- exercise1.py
+++ exercise1.py
@@ -1,5 +1,20 @@
+#open filed called input 
 file = open("input.txt")
-for dna in file:
-trimmed_dna = dna[14:]
-print(dna)
 
+#print trimmed dna 
+for dna in file:
+
+   trimmed_dna = dna[14:]
+   print(dna)
+
+#open output file 
+
+output = open("trimmed.txt", "w")
+
+#trimmed length and printed after prcoessing 
+
+trimmed_length = len(trimmed_dna)- 1
+
+output.write(trimmed_dna)
+
+print("processed sequence with length " + str(trimmed_length))
