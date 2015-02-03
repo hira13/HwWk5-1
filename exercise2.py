4f3f16c5b3991033189edf076f7756b5c18cc2b0
hirangi <hira13@gmail.com>
2015-02-03 01:49:19 +0000

    Added the rest of the sequence with the positions and line splits and saved it to another output file

--- exercise2.py
+++ exercise2.py
@@ -5,4 +5,24 @@ genomic_dna = open("genomic_dna.txt").read()
 
 exon_locations = open("exons.txt")
 
+#created a variable to hold the sequence 
+
+coding_sequence = ""
+
+for line in exon_locations:
+
+#split the line with a comma 
+      positions = line.split(',')
+#got the positions 
+      start = int(positions[0])
+      stop = int(positions[1])
+#took the exons from the dna 
+      exon = genomic_dna[start:stop]
+#added the exons to the cooding sequence
+      coding_sequence = coding_sequence + exon
+#inputed it to another file 
+
+output = open("coding_sequence.txt", "w")
+output.write(coding_sequence)
+output.close()
 
